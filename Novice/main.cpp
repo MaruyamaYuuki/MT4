737a68f867980171b168048c9cc091fc4bf3d18a
MaruyamaYuuki <k023g0106@g.neec.ac.jp>
2024-10-29 15:15:13 +0900

    01_02_確認課題

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -3,8 +3,12 @@
 #include"math/Matrix4x4.h"
 #include"math/Vector3.h"
 #include "math/MathUtility.h"
+#include <assert.h>
+
+using namespace KamataEngine;
 
 const char kWindowTitle[] = "LE2C_24_マルヤマ_ユウキ";
+
 KamataEngine::Matrix4x4 MakeRotateAxisAngle(const KamataEngine::Vector3& axis, float angle) {
 	KamataEngine::Matrix4x4 result = KamataEngine::MathUtility::MakeIdentityMatrix();
 	result.m[0][0] = (axis.x * axis.x) * (1 - std::cos(angle)) + std::cos(angle);
@@ -18,6 +22,56 @@ KamataEngine::Matrix4x4 MakeRotateAxisAngle(const KamataEngine::Vector3& axis, f
 	result.m[2][0] = axis.x * (axis.z * (1 - std::cos(angle))) + (axis.y * std::sin(angle));
 	result.m[2][1] = axis.y * (axis.z * (1 - std::cos(angle))) - (axis.x * std::sin(angle));
 	result.m[2][2] = (axis.z * axis.z) * (1 - std::cos(angle)) + std::cos(angle);
+	return result;
+}
+
+KamataEngine::Matrix4x4 DirectionToDiretion(const KamataEngine::Vector3& from, const KamataEngine::Vector3& to) {
+
+	KamataEngine::Vector3 num = KamataEngine::MathUtility::Cross(from, to);
+	float cos = KamataEngine::MathUtility::Dot(from, to);
+	float sin = KamataEngine::MathUtility::Length(num);
+
+	float epsilon = 1e-6f;
+	KamataEngine::Vector3 axis = {};
+
+	if (std::abs(cos + 1.0f) <= epsilon) {
+		// 反対方向のベクトルに回転する場合の処理
+		if (std::abs(from.x) > epsilon || std::abs(from.y) > epsilon) {
+			// (ux≠0||uy≠0) の際の axis の値を入れる 
+			axis.x = from.y;
+			axis.y = -from.x;
+			axis.z = 0.0f;
+		}
+		else if (std::abs(from.x) > epsilon || std::abs(from.z) > epsilon) {
+			// (ux≠0||uz≠0) の際の axis の値を入れる 
+			axis.x = from.z;
+			axis.y = 0.0f;
+			axis.z = -from.x;
+		}
+		else {
+			// zero vector 
+			assert(false);
+		}
+	}
+	else {
+		// それ以外の通常のケース
+		axis = KamataEngine::MathUtility::Normalize(num);
+	}
+
+	axis = KamataEngine::MathUtility::Normalize(axis);
+
+	KamataEngine::Matrix4x4 result = KamataEngine::MathUtility::MakeIdentityMatrix();
+	result.m[0][0] = (axis.x * axis.x) * (1 - cos) + cos;
+	result.m[0][1] = axis.x * (axis.y * (1 - cos)) + (axis.z * sin);
+	result.m[0][2] = axis.x * (axis.z * (1 - cos)) - (axis.y * sin);
+
+	result.m[1][0] = axis.x * (axis.y * (1 - cos)) - (axis.z * sin);
+	result.m[1][1] = (axis.y * axis.y) * (1 - cos) + cos;
+	result.m[1][2] = axis.y * (axis.z * (1 - cos)) + (axis.x * sin);
+
+	result.m[2][0] = axis.x * (axis.z * (1 - cos)) + (axis.y * sin);
+	result.m[2][1] = axis.y * (axis.z * (1 - cos)) - (axis.x * sin);
+	result.m[2][2] = (axis.z * axis.z) * (1 - cos) + cos;
 	return result;
 }
 
@@ -41,9 +95,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
-	KamataEngine::Vector3 num = { 1.0f,1.0f,1.0f };
-	KamataEngine::Vector3 axis = KamataEngine::MathUtility::Normalize(num);
-	float angle = 0.44f;
+
+	Vector3 fromNum0 = { 1.0f,0.7f,0.5f };
+	Vector3 fromNum1 = { -0.6f,0.9f,0.2f };
+	Vector3 toNum1 = { 0.4f,0.7f,-0.5f };
+	Vector3 num1 = { 1.0f,0.0f,0.0f };
+	Vector3 num2 = { -1.0f,0.0f,0.0f };
+
+	Vector3 from0 = MathUtility::Normalize(fromNum0);
+	Vector3 to0; to0.x = -from0.x; to0.y = -from0.y; to0.z = -from0.z;
+	Vector3 from1 = MathUtility::Normalize(fromNum1);
+	Vector3 to1 = MathUtility::Normalize(toNum1);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -58,7 +120,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		KamataEngine::Matrix4x4 rotateMatrix = MakeRotateAxisAngle(axis, angle);
+		Matrix4x4 rotateMatrix0 = DirectionToDiretion(MathUtility::Normalize(num1), MathUtility::Normalize(num2));
+		Matrix4x4 rotateMatrix1 = DirectionToDiretion(from0, to0);
+		Matrix4x4 rotateMatrix2 = DirectionToDiretion(from1, to1);
 
 		///
 		/// ↑更新処理ここまで
@@ -68,7 +132,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		MatrixScreenPrint(0, 0, rotateMatrix, "rotateMatrix");
+		MatrixScreenPrint(0, 0, rotateMatrix0, "rotateMatrix0");
+		MatrixScreenPrint(0, kRowHeight * 5, rotateMatrix1, "rotateMatrix1");
+		MatrixScreenPrint(0, kRowHeight * 10, rotateMatrix2, "rotateMatrix2");
 
 		///
 		/// ↑描画処理ここまで
